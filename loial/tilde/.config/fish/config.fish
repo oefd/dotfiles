4b890456f1b8a39bdd1a0ba8b8d733ab28963d9c
Terry Kerr <terry.kerr@shopify.com>
2022-06-29 12:44:19 -0400

    update pyenv

--- loial/tilde/.config/fish/config.fish
+++ loial/tilde/.config/fish/config.fish
@@ -7,6 +7,10 @@ if test -f /opt/dev/dev.fish
     source /opt/dev/dev.fish
 end
 
+pyenv init - | source
+# hack to prevent homebrew linking pyenv python versions
+alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+
 if status is-interactive
     alias ls="gls --color=auto --hide=Pictures --hide=Desktop --hide=Documents --hide=Music --hide=Public --hide=Movies --hide=Applications --hide=Downloads --hide=Library"
 end
